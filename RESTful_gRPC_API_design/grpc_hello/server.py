8eb8bb2bc2f407fbad24d90fa2dcb1751f1af9a9
Jonathan <jonathanchen301@gmail.com>
2025-10-28 12:05:13 -0400

    Reflection enabled

--- RESTful_gRPC_API_design/grpc_hello/server.py
+++ RESTful_gRPC_API_design/grpc_hello/server.py
@@ -1,24 +1,36 @@
 import grpc
 from concurrent import futures
+from grpc_reflection.v1alpha import reflection
 
 import generated.greeter_pb2_grpc as greeter_pb2_grpc
 import generated.greeter_pb2 as greeter_pb2
 
+
 class HelloService(greeter_pb2_grpc.SayHelloServicer):
     def SayHello(self, request, context):
         name = request.name
+        return greeter_pb2.SayHelloResponse(response=f"Hello, {name}!")
 
-        return greeter_pb2.SayHelloResponse(
-            response = f"Hello, {name}!"
-        )
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor())
     greeter_pb2_grpc.add_SayHelloServicer_to_server(HelloService(), server)
+
+    SERVICE_NAMES = (
+        greeter_pb2.DESCRIPTOR.services_by_name['SayHello'].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
+
     server.add_insecure_port("[::]:50051")
     server.start()
-    print("gRPC server running on port 50051")
-    server.wait_for_termination()
+    print("gRPC server running on port 50051 with reflection enabled")
+    try:
+        server.wait_for_termination()
+    except KeyboardInterrupt:
+        print("Shutting down server...")
+        server.stop(0)
+
 
 if __name__ == "__main__":
     serve()
